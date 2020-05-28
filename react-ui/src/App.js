7cf50215cf56d59c0a5cf34e8f0798c50c3cf199
ajhswan <andrewswan.dev@gmail.com>
2020-05-28 15:21:56 +0100

    cleaned up app file and fixed ts any errors

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,47 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 function App() {
-    const [message, setMessage] = useState(null);
-    const [isFetching, setIsFetching] = useState(false);
-    const url = useState('/api');
-    const fetchData = useCallback(() => {
-        fetch(url)
-            .then(response => {
-            if (!response.ok) {
-                throw new Error(`status ${response.status}`);
-            }
-            return response.json();
-        })
-            .then(json => {
-            setMessage(json.message);
-            setIsFetching(false);
-        }).catch(e => {
-            setMessage(`API call failed: ${e}`);
-            setIsFetching(false);
-        });
-    }, [url]);
-    useEffect(() => {
-        setIsFetching(true);
-        fetchData();
-    }, [fetchData]);
     return (React.createElement("div", { className: "App" },
-        React.createElement("header", { className: "App-header" },
-            React.createElement("img", {className: "App-logo", alt: "logo" }),
-            process.env.NODE_ENV === 'production' ?
-                React.createElement("p", null, "This is a production build from create-react-app.")
-                : React.createElement("p", null,
-                    "Edit ",
-                    React.createElement("code", null, "src/App.js"),
-                    " and save to reload."),
-            React.createElement("p", null,
-                '« ',
-                React.createElement("strong", null, isFetching
-                    ? 'Fetching message from API'
-                    : message),
-                ' »'),
-            React.createElement("p", null,
-                React.createElement("a", { className: "App-link", href: "https://github.com/mars/heroku-cra-node" }, "React + Node deployment on Heroku")),
-            React.createElement("p", null,
-                React.createElement("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, "Learn React")))));
+        React.createElement("header", { className: "App-header" })));
 }
 export default App;
