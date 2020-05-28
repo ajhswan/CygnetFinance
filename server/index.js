62bd28c5df43ce3468e4ba29262d692f819c8887
ajhswan <andrewswan.dev@gmail.com>
2020-05-28 15:44:15 +0100

    moved project dependencies to devDependencies in package.json and fixed index.js to rever to build folder

--- server/index.js
+++ server/index.js
@@ -17,13 +17,13 @@ if (!isDev && cluster.isMaster) {
 }
 else {
     const app = express();
-    app.use(express.static(path.resolve(__dirname, '../react-ui/src')));
+    app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
     app.get('/api', function (req, res) {
         res.set('Content-Type', 'application/json');
         res.send('{"message":"Hello from the custom server!"}');
     });
     app.get('*', function (request, response) {
-        response.sendFile(path.resolve(__dirname, '../react-ui/src', 'index.html'));
+        response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
     });
     app.listen(PORT, function () {
         console.error(`Node ${isDev ? 'dev server' : 'cluster worker ' + process.pid}: listening on port ${PORT}`);
