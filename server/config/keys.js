94e95376b72adbf6a993269deb89bbfbc8941546
ajhswan <andrewswan.dev@gmail.com>
2020-06-11 15:35:22 +0100

    removed config files for heroku deployment- it is working

--- server/config/keys.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.keys = {
-    MONGODB_URI: 'mongodb://heroku_rn0m35kx:7GypQ6OFKokgxBgcF4yIWO59ibkSlJ@ds239368.mlab.com:39368/heroku_rn0m35kx',
-    secretOrKey: "secret"
-};
-module.exports.keys = exports.keys;
