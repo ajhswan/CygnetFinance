e57299b813cfeb42441837161d4ebf454ac245cf
ajhswan <andrewswan.dev@gmail.com>
2020-06-18 12:51:48 +0100

    adding config files for deployment on heroku

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.keys = {
+    MONGODB_URI: "mongodb://heroku_rn0m35kx:StefHurry1@ds239368.mlab.com:39368/heroku_rn0m35kx",
+    secretOrKey: "secret"
+};
+module.exports.keys = exports.keys;
